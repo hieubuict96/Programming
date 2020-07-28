c1e0ac482f41ec62139ac5bc73dd8760fa31623d
HIEUBUI <hieubuict96@gmail.com>
2020-07-28 10:16:38 +0700

    g

---
+++ react_training - Copy/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar.js'
+import Index from './components/Index'
+import Detail from './components/Detail'
+import Cart from './components/Cart'
+import Contact from './components/Contact'
+import { Switch, Route } from 'react-router-dom'
+import 'project_react/bootstrap.min.css'
+
+class App extends Component {
+	render(){
+		return(
+			<React.Fragment>
+				<NavBar />
+				<Switch>
+					<Route exact path=""  component= {Index} />
+					<Route exact path="/detail"  component={Detail} />
+					<Route exact path="/cart"   component={Cart} />
+					<Route exact path="/contact"  component={Contact} />
+				</Switch>
+			</React.Fragment>
+		)
+	}
+}
+
+export default App;
---
+++ react_training - Copy/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg'
+import ButtonContainer from 'ButtonContainer'
+
+class NavBar extends Component {
+    render(){
+        return(
+            <nav>
+                <ul>
+                    <li>
+                        <img src={logo} />
+                    </li>
+                    <li>
+                        <Link to="">Product</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">
+                            <ButtonContainer></ButtonContainer>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar;
