3a7ec943c3bd3512e69803318a4760bc5d43383e
hieubuict96 <56767246+hieubuict96@users.noreply.github.com>
2020-08-04 21:56:37 +0700

    21:56

--- react_training/src/components/Detail.js
+++ react_training/src/components/Detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { detailProduct } from "../data"
 import {ButtonContainer} from "./ButtonContainer"
+import { Link } from "react-router-dom"
 
 export default class Detail extends Component {
     render() {
@@ -12,8 +13,18 @@ export default class Detail extends Component {
                         <p><span className="text-capitalize">product name: </span><span>{detailProduct.title}</span></p>
                         <p className="text-uppercase"><span>company: </span><span>{detailProduct.company}</span></p>
                         <p><span className="text-capitalize">price: </span><span>{detailProduct.price} $</span></p>
-                        <p className="text-capitalize">some info about product: </p>
+                        <p className="text-capitalize font-w">some info about product: </p>
                         <p className="text-capitalize">{detailProduct.info}</p>
+                        <Link to="/">
+                            <ButtonContainer className="text-capitalize mr-4">
+                                back to products
+                            </ButtonContainer>
+                        </Link>
+                        <Link to="/">
+                            <ButtonContainer className="text-capitalize" disabled={detailProduct.inCart}>
+                                {detailProduct.inCart ? "in cart" : "add to cart"}
+                            </ButtonContainer>
+                        </Link>
                     </div>
                 </div>
             </div>
