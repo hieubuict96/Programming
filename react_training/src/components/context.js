626ec3d0bcdb5e0fd96b2428a1fd380faf062df5
HIEUBUI <hieubuict96@gmail.com>
2020-08-06 16:38:34 +0700

    y

--- react_training/src/components/context.js
+++ react_training/src/components/context.js
@@ -1,8 +1,32 @@
 import {storeProducts} from '../data.js'
-import React from 'react'
+import React, { Component } from 'react'
 
 const ContextProduct = React.createContext(storeProducts);
 
+class ProductProvider extends Component {
+    state = {
+        products: [],
+        cart: [],
+        detailProduct: [],
+        cartTotal: 0,
+        subTotal: 0
+    }
+
+    addToCart = () => {
+
+    }
+
+    render() {
+        return (
+            <ContextProduct.Provider value={storeProducts
+
+            }>
+
+            </ContextProduct.Provider>
+        )
+    }
+}
+
 const ContextConsumer = ContextProduct.Consumer;
 
-export {ContextProduct, ContextConsumer};
+export {ProductProvider, ContextConsumer};
