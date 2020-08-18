346f099e3cc62f515e4020e5caf9496eecf587c7
hieubuict96 <hieubuict96@gmail.com>
2020-08-18 09:56:30 +0700

    fd

--- react_training/src/components/Cart/Cart.js
+++ react_training/src/components/Cart/Cart.js
@@ -1,26 +1,81 @@
 import React, { Component } from "react";
 import { ProductConsumer } from "../context";
 import CartItem from "./CartItem";
+import { ButtonContainer } from "../ButtonContainer";
 
 export default class Cart extends Component {
     render() {
         return (
-            <React.Fragment>
+            <div className="container">
                 <h1 className="text-uppercase text-center mt-5">your cart</h1>
                 <ProductConsumer>
-                    {context => {
+                    {(context) => {
                         if (context.cart.length > 0) {
-                            return(
-                                context.cart.map(value => {
-                                    return <CartItem {...value} />
-                                })
-                            )
+                            const totalItem = context.cart.map((value, key) => {
+                                return <CartItem {...value} key={key} />;
+                            });
+                            return (
+                                <>
+                                    <div className="d-flex mb-2">
+                                        <span className="text-uppercase col-2 text-center">
+                                            products
+                                        </span>
+                                        <span className="col-2 text-uppercase text-center">
+                                            name of product
+                                        </span>
+                                        <span className="col-2 text-uppercase text-center">
+                                            price
+                                        </span>
+                                        <span className="col-2 text-uppercase text-center">
+                                            quantity
+                                        </span>
+                                        <span className="col-2 text-uppercase text-center">
+                                            remove
+                                        </span>
+                                        <span className="col-2 text-center">
+                                            total
+                                        </span>
+                                    </div>
+                                    {totalItem}
+                                    <div className="col-3 ml-auto">
+                                        <p className="text-right">
+                                            <ButtonContainer className="text-uppercase" onClick={context.clearCart}>
+                                                clear cart
+                                            </ButtonContainer>
+                                        </p>
+                                        <p className="font-weight-bold text-right">
+                                            <span className="text-uppercase">
+                                                sub total :
+                                            </span>
+                                            <span>
+                                                {context.cartSubTotal} $
+                                            </span>
+                                        </p>
+                                        <p className="font-weight-bold text-right">
+                                            <span className="text-uppercase">
+                                                tax :
+                                            </span>
+                                            <span>{context.cartTax} $</span>
+                                        </p>
+                                        <p className="font-weight-bold text-right">
+                                            <span className="text-uppercase">
+                                                total :
+                                            </span>
+                                            <span>{context.cartTotal} $</span>
+                                        </p>
+                                    </div>
+                                </>
+                            );
                         } else {
-                            return <h5 className="text-capitalize text-center mt-5">your cart is empty</h5>
+                            return (
+                                <h5 className="text-capitalize text-center mt-5">
+                                    your cart is empty
+                                </h5>
+                            );
                         }
                     }}
                 </ProductConsumer>
-            </React.Fragment>
+            </div>
         );
     }
 }
--- react_training/src/components/Cart/CartItem.js
+++ react_training/src/components/Cart/CartItem.js
@@ -1,33 +1,38 @@
 import React, { Component } from "react";
-import {ProductConsumer} from "../context"
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
 
 class CartItem extends Component {
     render() {
         return (
-            <div>
             <ProductConsumer>
-                {context => (
-                    <div className="d-flex">
-                    <img src={this.props.img} className="col-2" alt="img" />
-                    <span className="col-2">{this.props.title}</span>
-                    <span className="col-2">{this.props.price}</span>
-                    <span className="col-2">
-                        <button className="p-2" onClick={context.decrement}>-</button>
-                        <span className="p-2">{this.props.count}</span>
-                        <button className="p-2" onClick={context.increment}>+</button>
-                    </span>
-                    <span className="p-2">
-                        <i class="fas fa-trash"></i>
-                    </span>
-                    <span className="p-2">
-                        <span className="text-capitalize">item total: </span>
-                        <span>{this.props.total} $</span>
-                    </span>
-                </div>
+                {(context) => (
+                    <div className="d-flex mb-3">
+                        <img src={this.props.img} className="col-2 text-center" alt="img" />
+                        <span className="col-2 text-center align-self-center">{this.props.title}</span>
+                        <span className="col-2 text-center align-self-center">{this.props.price} $</span>
+                        <span className="col-2 text-center align-self-center">
+                            <button className="px-2 py-0" onClick={() => context.decrement(this.props.id)}>
+                                -
+                            </button>
+                            <span className="p-2">{this.props.count}</span>
+                            <button className="px-2 py-0" onClick={() => context.increment(this.props.id)}>
+                                +
+                            </button>
+                        </span>
+                        <span className="col-2 text-center align-self-center">
+                            <Link to="/cart" onClick={() => context.removeItem(this.props.id)}>
+                                <i className="fas fa-trash"></i>
+                            </Link>
+                        </span>
+                        <span className="col-2 text-center align-self-center">
+                            <span className="text-capitalize">item total:</span>
+                            <span>{this.props.total} $</span>
+                        </span>
+                    </div>
                 )}
             </ProductConsumer>
-            </div>
-        )
+        );
     }
 }
 
